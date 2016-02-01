4649499c54125b950f547b2b3f52b6b188549c37
spasynkov <s.pasynkov@gmail.com>
2016-02-02 00:18:07 +0200

    multithreading features added

--- src/Client.java
+++ src/Client.java
@@ -6,23 +6,51 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client {
-    public int counter = 0;
+    private static String defaultHost = "localhost";
+
     public static void main(String[] args) throws IOException {
+        //runSeveralThreads();
+        askUser();
+    }
+
+    private static void askUser() throws IOException {
         String s;
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter IP or domain name to connect: ");
+        String host = consoleReader.readLine();
 
+        do {
+            System.out.print("Enter your message (type exit to leave): ");
+            s = consoleReader.readLine();
+
+            if (host == null || host.isEmpty()) host = defaultHost;
+            InetAddress ip = InetAddress.getByName(host);
+            Socket socket = new Socket(ip, 6789);
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+
+            outToServer.write(s.getBytes("UTF-8"));
+            System.out.println("Sent.");
+            System.out.println(inFromServer.readLine());
+            socket.close();
+        } while (!s.toLowerCase().equals("exit"));
+
+        consoleReader.close();
+    }
+
+    private static void runSeveralThreads() {
         for (int i = 0; i < 10000; i++) {
             new Thread("test_thread_" + (i + 1)){
                 @Override
                 public void run() {
                     try {
-                        InetAddress ip = InetAddress.getByName("192.168.0.2");
+                        InetAddress ip = InetAddress.getByName(defaultHost);
                         Socket socket = new Socket(ip, 6789);
                         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                         DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
 
                         String s = Thread.currentThread().getName();
-                        outToServer.write((s + "\n").getBytes("UTF-8"));
+                        outToServer.write(s.getBytes("UTF-8"));
                         System.out.println(inFromServer.readLine());
                         socket.close();
                     } catch (Exception e) {
@@ -31,22 +59,5 @@ public class Client {
                 }
             }.start();
         }
-
-/*        System.out.print("Enter IP or domain name to connect: ");
-        String sIP = consoleReader.readLine();
-        if (sIP == null || sIP.isEmpty()) sIP = "ff.ddns.ukrtel.net";
-        InetAddress ip = InetAddress.getByName(sIP);
-        Socket socket = new Socket(ip, 6789);
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
-
-        System.out.print("Enter your message: ");
-        s = consoleReader.readLine();
-        outToServer.write((s + "\n").getBytes("UTF-8"));
-        System.out.println("Sent.");
-        System.out.println(inFromServer.readLine());
-        socket.close();
-*/
-        consoleReader.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,33 +1,78 @@
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        String s;
         ServerSocket serverSocket = new ServerSocket(6789);
         SimpleDateFormat format = new SimpleDateFormat("H:mm:ss:SSS EEE, d MMMM y ('GMT' XXX)");
+        long clientNumber = 0;
 
         while (true) {
             try {
-                System.out.println(format.format(new Date()) + "\tListening...");
-                Socket socket = serverSocket.accept();
-                InputStream inFromClient = socket.getInputStream();
-                PrintWriter outToClient = new PrintWriter(socket.getOutputStream());
-                s = getMessageFromClient(inFromClient);
-                System.out.println();
-                outToClient.write(s);
+                System.out.println(format.format(System.currentTimeMillis()) + "\tListening...");
+                new Replier(serverSocket.accept(), ++clientNumber, format).start();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private static String getMessageFromClient(InputStream inputStream) {
-        return "";
+    private static class Replier extends Thread {
+        private Socket socket;
+        private long clientNumber;
+        private SimpleDateFormat format;
+
+        Replier(Socket socket, long clientNumber, SimpleDateFormat dateFormat) {
+            this.socket = socket;
+            this.clientNumber = clientNumber;
+            format = dateFormat;
+        }
+
+        @Override
+        public void run() {
+            InputStream inFromClient = null;
+            PrintWriter outToClient = null;
+            try {
+                System.out.println(format.format(System.currentTimeMillis()) + "\tClient #" + clientNumber + " connected.");
+                inFromClient = socket.getInputStream();
+                outToClient = new PrintWriter(socket.getOutputStream());
+                String s = getMessageFromClient(inFromClient);
+                System.out.println(format.format(System.currentTimeMillis()) + "\tGot message from client #" + clientNumber + ": \"" + s + "\"\tSending back...");
+                outToClient.write(s.toUpperCase());
+                System.out.println(format.format(System.currentTimeMillis()) + "\tSent to client #" + clientNumber + ".\n");
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                close(outToClient, inFromClient, socket);
+            }
+        }
+
+        private void close(Closeable... streams) {
+            for (Closeable stream : streams) {
+                try {
+                    stream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private String getMessageFromClient(InputStream inputStream) throws IOException {
+            byte[] buffer = new byte[1024];
+            int bufferSize;
+            StringBuffer sb = new StringBuffer();
+
+            while (inputStream.available() > 0) {
+                bufferSize = inputStream.read(buffer);
+                for (int i = 0; i < bufferSize; i++)
+                    sb.append((char) buffer[i]);
+            }
+            return sb.toString();
+        }
     }
 }
